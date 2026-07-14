59de41a7db286ce331f76498f4e7ea5e74594744
Kye Cunningham <kyecunningham53@gmail.com>
2026-07-14 18:00:03 +0100

    fixing editor issues

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -52,7 +52,7 @@ public class Enemy : MonoBehaviour
         m_CurrentHealth = m_MaxHealth;
         m_DamageTick = 0;
     }
-    void TakeDamage(int amount)
+    public void TakeDamage(int amount)
     {
         m_CurrentHealth -= amount;
 
@@ -61,7 +61,7 @@ public class Enemy : MonoBehaviour
             Die();
         }
     }
-    void Die()
+    public void Die()
     {
         OnEnemyDied(this);
         Destroy(gameObject);
